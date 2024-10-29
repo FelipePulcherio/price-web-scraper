ce55e2ad8da0759f81c37f412ce9cefe458f9bd4
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-10-28 17:22:37 -0700

    Create schema and export as Model

---
+++ backend/src/db/schemas/history.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+import { IHistory } from '../../types/types';
+
+// Create Schema
+const historySchema = new Schema<IHistory>(
+  {
+    _id: { type: Schema.ObjectId },
+    item_id: { type: Schema.ObjectId, ref: 'Item', required: true },
+    data_full: [
+      {
+        store: { type: String, required: true },
+        price: { type: Number, required: true },
+        date: { type: Date, default: Date(), required: true },
+        moment: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    collection: 'History',
+  }
+);
+
+// Create and export model using historySchema
+module.exports = model('History', historySchema);

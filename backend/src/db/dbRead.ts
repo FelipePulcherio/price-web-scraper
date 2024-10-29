cd2888ea0a7642087a8d12f646ad3f398323009a
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-10-28 17:58:13 -0700

    Initialize file with imports and consts

---
+++ backend/src/db/dbRead.ts
@@ -0,0 +1,11 @@
+import { connect, connection } from 'mongoose';
+import dotenv from 'dotenv';
+import { IHistory, IItem } from '../types/types';
+import { Item, History } from './models/models';
+
+// Read .env file
+dotenv.config();
+
+const dbName = process.env.DATABASE_NAME || '';
+const uri = process.env.DATABASE_URL || '';
+const mongoDBUri = uri.split('?')[0] + dbName + '?' + uri.split('?')[1];

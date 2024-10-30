c475281ed465c8c3d738b1aeef2213f4691ffafd
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-10-29 19:06:05 -0700

    Create function updateHistoryCollection

--- backend/src/db/dbUpdate.ts
+++ backend/src/db/dbUpdate.ts
@@ -1,6 +1,6 @@
-import { connect, connection } from 'mongoose';
+import { AnyBulkWriteOperation, connect, connection, Schema } from 'mongoose';
 import dotenv from 'dotenv';
-import { IHistory, IItem } from '../types/types';
+import { IHistory, IItem, IShortItem, IScraperResult } from '../types/types';
 import { Item, History, Graph } from './models/models';
 
 // Read .env file
@@ -9,3 +9,80 @@ dotenv.config();
 const dbName = process.env.DATABASE_NAME || '';
 const uri = process.env.DATABASE_URL || '';
 const mongoDBUri = uri.split('?')[0] + dbName + '?' + uri.split('?')[1];
+
+// FUNCTIONS
+// Define interface for props used in updateHistoryCollection function
+interface updateHistoryCollectionProps {
+  listOfSuccessObj: IShortItem[];
+  retryCount?: number;
+}
+
+async function updateHistoryCollection({
+  listOfSuccessObj,
+  retryCount = 3,
+}: updateHistoryCollectionProps) {
+  // Accept array in format: [ { _id: , url: , price: , lastUpdated: , status: , store: } ]
+
+  // Connect to MongoDB with mongoose
+  try {
+    // Start connection
+    await connect(mongoDBUri);
+    console.log('Db_update: Connected to MongoDB.');
+
+    // Declare bulkOperations array
+    const bulkOperations: AnyBulkWriteOperation[] = [];
+
+    // Iterate over listOfSuccessObj and create a new Entry for each one
+    listOfSuccessObj.forEach(async (item) => {
+      const newEntry: IScraperResult = {
+        store: item.storeName,
+        price: item.price,
+        date: item.lastUpdated,
+        moment: item.lastUpdated.getHours() < 22 ? 'Morning' : 'Evening',
+      };
+
+      // Prepare update operation for each item
+      bulkOperations.push({
+        updateOne: {
+          filter: { item_id: item._id },
+          update: { $push: { data_full: newEntry } },
+        },
+      });
+    });
+
+    try {
+      // Execute bulk update operation
+      await History.bulkWrite(bulkOperations);
+
+      console.log('Db_update: History update success!\nUpdated IDs:');
+      // listOfSuccessObj.forEach((item) => console.log(item._id));
+      //
+    } catch (error) {
+      console.error(
+        `Db_update: Error executing bulk write operation in History: ${error}`
+      );
+
+      // Check if it's possible to try again.
+      if (retryCount > 0) {
+        console.log(
+          `Db_update: Retrying update operation.\nRetries left: ${retryCount}`
+        );
+
+        // Retry the operation recursively with decremented retryCount
+        await updateHistoryCollection({
+          listOfSuccessObj,
+          retryCount: retryCount - 1,
+        });
+      } else {
+        console.error('Db_update: Retry limit reached. Operation failed.');
+        // Need more error Handling here! (Logging)
+      }
+    }
+  } catch (err) {
+    // Error handling related to connection
+    console.log(`Db_update: Error connecting to the database: ${err}`);
+  } finally {
+    await connection.close();
+    console.log('Db_update: Mongoose closed.');
+  }
+}

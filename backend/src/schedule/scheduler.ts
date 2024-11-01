1e9572a38b9cc9dbc8ab83a618859e7665edf791
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-11-01 07:50:31 -0700

    Add basic structure of scheduled jobs

--- backend/src/schedule/scheduler.ts
+++ backend/src/schedule/scheduler.ts
@@ -1,5 +1,7 @@
-import Agenda from 'agenda';
+import { Agenda, Job } from '@hokify/agenda';
 import { mongoConfig } from '../config/config';
+import { IItem, ISchedule } from '../types/types';
+import { readItemCollection } from '../db/operations/dbRead';
 import {
   updateHistoryCollection,
   updateGraphCollection,
@@ -8,5 +10,72 @@ import {
 
 // Create new instance of Agenda WITHOUT MONGOOSE
 const scraperAgenda = new Agenda({
-  db: { address: mongoConfig.MONGO_URI, collection: 'SchedulerJobs-scraper' },
+  db: { address: mongoConfig.MONGO_URI, collection: 'Scheduler-jobs' },
 });
+
+// Define an interface for job "fetcher" and "scraper" data
+interface FetcherJobData {
+  fetcherData: IItem[];
+}
+
+// Define new job for fetching data: "fetcher"
+scraperAgenda.define('fetcher', async (job: Job<FetcherJobData>) => {
+  console.log('Scheduler: Job 1 "fetcher" running...');
+
+  // Get information from DB
+  const allItems: IItem[] = await readItemCollection({
+    interestFields: '_id stores isActive lowestPrice',
+  });
+
+  // Store the result in job data
+  job.attrs.data = { fetcherData: allItems };
+  await job.save();
+});
+
+// Define new job for scraping data: "scraper"
+scraperAgenda.define('scraper', async (job: Job<FetcherJobData>) => {
+  console.log('Scheduler: Job 2 "scraper" Running...');
+
+  // Access data passed from job "fetcher"
+  const { fetcherData } = job.attrs.data;
+
+  // console.log('Data received on "scraper" job:');
+  // console.log('Total Items: ', fetcherData.length);
+  // console.log(fetcherData[0]);
+});
+
+// Schedule the "fetcher" job to run every X hours
+const runJobs = async () => {
+  await scraperAgenda.every('15 seconds', 'fetcher');
+};
+
+// Set up listeners for completion
+scraperAgenda.on('complete', async (job) => {
+  // 'fetcher' job triggers 'scraper' job on completion
+  if (job.attrs.name === 'fetcher') {
+    console.log('Scheduler: Job 1 "fetcher" completed.');
+    const fetcherData = (job.attrs.data as FetcherJobData).fetcherData; // Type-cast to FetcherJobData
+    await scraperAgenda.now('scraper', { fetcherData });
+  }
+
+  if (job.attrs.name === 'scraper') {
+    console.log('Scheduler: Job 2 "scraper" completed.');
+  }
+});
+
+// Start Agenda
+(async () => {
+  await scraperAgenda.start();
+  await runJobs();
+  console.log('Scheduler: Agenda started!');
+})();
+
+// Handle graceful shutdown (.on)
+async function gracefulShutdown() {
+  await scraperAgenda.stop();
+  console.log('Scheduler: Agenda stopped!');
+  process.exit(0);
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);

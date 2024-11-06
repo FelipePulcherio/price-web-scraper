c5650efdb904a2c76ea7eacf1c8ece02ec967e97
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2024-11-06 11:14:19 -0800

    Add deleteScraperJobsCollection function

---
+++ backend/src/db/operations/dbDelete.ts
@@ -0,0 +1,46 @@
+import { connect, connection } from 'mongoose';
+import { mongoConfig } from '../../config/config';
+
+// FUNCTIONS
+// Define interface for props used in updateHistoryCollection function
+interface deleteScraperJobsCollectionProps {
+  retryCount?: number;
+}
+
+export async function deleteScraperJobsCollection({
+  retryCount = 3,
+}: deleteScraperJobsCollectionProps): Promise<void> {
+  // Connect to MongoDB with mongoose
+  try {
+    await connect(mongoConfig.MONGO_URI, mongoConfig.MONGO_CONNECT_OPTIONS);
+    console.log('Db_delete: Connected to MongoDB.');
+
+    const schedulerCollection = await connection.db
+      .listCollections({ name: 'Scheduler-jobs' })
+      .hasNext();
+
+    // Check if collection exists
+    if (schedulerCollection) {
+      const deleteResult = await connection.db
+        .collection('Scheduler-jobs')
+        .deleteMany({ name: 'fetcher' });
+
+      if (deleteResult.deletedCount > 0) {
+        console.log(
+          `Db_delete: Deleted ${deleteResult.deletedCount} documents with name "fetcher" in the "Scheduler-jobs" collection.`
+        );
+      } else {
+        console.log(
+          'Db_delete: No documents with name "fetcher" were found in the "Scheduler-jobs" collection.'
+        );
+      }
+    } else {
+      console.log('Db_delete: "Scheduler-jobs" collection was not found.');
+    }
+  } catch (err) {
+    console.error(`Db_delete: Error connecting to the database: ${err}`);
+  } finally {
+    await connection.close();
+    console.log('Db_delete: Mongoose connection closed.');
+  }
+}

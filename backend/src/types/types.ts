34646f88c75effac00be7cd1ac6967a0ad3ffee9
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-04 15:32:56 -0800

    Update types to relflect db schema changes

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,69 +1,56 @@
-import { Types } from 'mongoose';
+import { Prisma, Role } from '@prisma/client';
+
+// Define interface for Store
+export interface IStore {
+  id?: number;
+  name: string;
+  logo?: string;
+}
 
 // Define interface for store info inside Item
-export interface IStoreDetail {
+export interface IShortStore {
+  id?: number;
   name: string;
-  logo: string;
   url: string;
+}
+
+// Define interface for Category
+export interface ICategory {
+  id?: number;
+  name: string;
 }
 
 // Define interface for item
-export interface IItem {
-  _id: Types.ObjectId;
-  name?: string;
-  brand?: string;
-  description?: string;
-  category?: string;
-  isActive?: Boolean;
-  stores: IStoreDetail[];
-  addDate?: Date;
-  lastUpdated: Date;
-  lowestPrice: number;
-  lowestStore: string;
-  searchCount?: number;
-}
-
-// Define interface for short item
 export interface IShortItem {
-  _id: Types.ObjectId;
-  url: string;
-  price: number;
-  lastUpdated: Date;
-  status: 'OK' | 'Failed' | null;
+  id?: number;
+  name: string;
+  model: string;
+  brand: string;
+  categories: ICategory[];
+  description?: Prisma.JsonObject;
+  stores: IShortStore[];
+}
+
+// Define interface for event
+export interface IEvent {
+  id?: number;
+  itemId?: number;
+  itemName: string;
+  storeId?: number;
   storeName: string;
-}
-
-// Define interface for store
-export interface IStore {
-  storeName: string;
-  items: IShortItem[];
-}
-
-// Define interface for scraped result
-export interface IScraperResult {
-  store: string;
   price: number;
-  date: Date;
-  moment: string;
+  date?: Date;
+  fromJob: string;
 }
 
-// Define interface for history
-export interface IHistory {
-  _id: Types.ObjectId;
-  item_id: Types.ObjectId;
-  dataFull: IScraperResult[];
-}
-
-// Define interface for graph result
-export interface IGraphResult {
-  lowestPrice: number;
-  lowestStore: string;
-  date: Date;
-}
-
-// Define interface for graph
-export interface IGraph {
-  _id: Types.ObjectId;
-  item_id: Types.ObjectId;
-  data365: IGraphResult[];
+// Define interface for user
+export interface IUser {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  phone: string;
+  password: string;
+  role: Role;
 }

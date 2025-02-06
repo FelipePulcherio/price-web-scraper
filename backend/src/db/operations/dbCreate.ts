1429f2fbde1968e130afb6df1af2e36d0621b017
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-05 16:45:46 -0800

    Define createUser function

---
+++ backend/src/db/operations/dbCreate.ts
@@ -0,0 +1,42 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+import { IUser, IShortUser } from '../../types/types';
+
+// FUNCTIONS
+export async function createUser(
+  prisma: PrismaClient,
+  data: IUser
+): Promise<IShortUser | undefined> {
+  try {
+    const { firstName, lastName, userName, email, phone, password } = data;
+
+    const newUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        userName,
+        email,
+        phone,
+        password,
+        updatedById: '01010101-ffff-1111-ffff-010101010101',
+      },
+    });
+
+    return { userName: newUser.userName, email: newUser.email };
+  } catch (error) {
+    // Throw error to whoever called this
+    // Handle errors from Prisma
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // Unique constraint error (userName and email)
+      if (error.code === 'P2002') {
+        throw new Error(`${error.meta?.target} already in use.`);
+      }
+    }
+    // All other errors
+    else {
+      console.error('Error creating user:', error);
+      throw new Error(
+        error instanceof Error ? error.message : 'Unknown error.'
+      );
+    }
+  }
+}

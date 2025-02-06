00adcf0f2a7f0220d9990ca0e4532ced038b9302
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-05 16:47:19 -0800

    Add route /api/v1/auth/register

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import { IAPI } from '../types/types';
+import { createUser } from '../db/operations/dbCreate';
 
 function apiResponse<T>(success: boolean, message: string, data: T): IAPI<T> {
   return {
@@ -16,15 +17,30 @@ const apiRoutes = (prisma: PrismaClient) => {
   const v1Router = express.Router();
 
   // POST /api/v1/auth/register
+  // Used when user try to create an account
   v1Router.post('/auth/register', async (req, res) => {
     try {
-      console.log('POST /api/v1/auth/register: Received.');
+      console.log('POST /api/v1/auth/register Request Body:');
+      console.log(req.body);
+
+      const newUser = await createUser(prisma, req.body);
       res
         .status(201)
-        .json(apiResponse(true, 'User registered successfully', {}));
+        .json(apiResponse(true, 'User registered successfully', newUser));
     } catch (error) {
-      console.error('Error creating user:', error);
-      res.status(500).json(apiResponse(false, 'Internal server error', {}));
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('POST /api/v1/auth/register', errorMessage);
+
+      // Errors
+      if (errorMessage.includes('already in use')) {
+        res.status(409).json(apiResponse(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res.status(500).json(apiResponse(false, 'Internal server error', null));
+      }
     }
   });
 

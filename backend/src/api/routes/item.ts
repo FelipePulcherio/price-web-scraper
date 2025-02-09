e5c6a921293495eaba57049913338f51d9426c91
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 13:16:46 -0800

    Add GET /api/v1/items/category/:categoryId

--- backend/src/api/routes/item.ts
+++ backend/src/api/routes/item.ts
@@ -1,5 +1,8 @@
 import { Router, Request, Response } from 'express';
-import { getItemById } from '@/database/operations/dbRead';
+import {
+  getItemById,
+  getItemsByCategoryId,
+} from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 
 const route = Router();
@@ -33,6 +36,40 @@ export default (app: Router) => {
       // Forbidden
       else if (errorMessage.includes('Forbidden')) {
         res.status(403).json(resFormatter(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res
+          .status(500)
+          .json(resFormatter(false, 'Internal server error', null));
+      }
+    }
+  });
+
+  // GET /api/v1/items/category/:categoryId
+  // Used to find all items that are related to a categoryId
+  route.get('/category/:categoryId', async (req: Request, res: Response) => {
+    try {
+      const categoryId = parseInt(req.params.categoryId, 10);
+      console.log(
+        `GET /api/v1/items/category/:categoryId Request param: ${categoryId}`
+      );
+
+      const fetchedItems = await getItemsByCategoryId(categoryId);
+      // console.log(fetchedItems);
+
+      res
+        .status(200)
+        .json(resFormatter(true, 'Items fetched successfully', fetchedItems));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('GET /api/v1/items/category/:categoryId', errorMessage);
+
+      // Not found
+      if (errorMessage.includes('Not found')) {
+        res.status(404).json(resFormatter(false, errorMessage, null));
       }
       // All other errors
       else {

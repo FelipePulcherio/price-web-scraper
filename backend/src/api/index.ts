35f706764d85d8ecc14f826ce6f0ce4b25eda4dc
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 13:48:57 -0800

    Import and use store route

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import user from './routes/user';
 import item from './routes/item';
 import category from './routes/category';
+import store from './routes/store';
 
 export default () => {
   const app = Router();
   user(app);
   item(app);
   category(app);
+  store(app);
 
   return app;
 };

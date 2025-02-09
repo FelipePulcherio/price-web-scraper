ef5f22390f04359d182d94c6623d1b465681a525
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 13:06:34 -0800

    Import and use category route

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import user from './routes/user';
 import item from './routes/item';
+import category from './routes/category';
 
 export default () => {
   const app = Router();
   user(app);
   item(app);
+  category(app);
 
   return app;
 };

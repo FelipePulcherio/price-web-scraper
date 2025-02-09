aa2bbc9127d2d85700405152bea1132216ef90aa
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-08 19:29:07 -0800

    Use 'item' route in app

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import user from './routes/user';
+import item from './routes/item';
 
 export default () => {
   const app = Router();
   user(app);
+  item(app);
 
   return app;
 };

e7887cd7aa9acf2d6f339736c0f9483159d75ffb
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-08 19:30:38 -0800

    Add function getItemById

--- backend/src/database/operations/dbRead.ts
+++ backend/src/database/operations/dbRead.ts
@@ -1,101 +1,79 @@
-import { mongoConfig } from '../../config/config';
-import { connect, connection, ObjectId, Types, FilterQuery } from 'mongoose';
-import { IHistory, IItem } from '../../types/types';
-import { Item, History } from '../models/models';
+import { Prisma } from '@prisma/client';
+import { IShortItem } from '@/interfaces/interfaces';
+import prisma from '@/loaders/prisma';
 
 // FUNCTIONS
-// Define interface for props used in readItemCollection function
-interface readItemCollectionProps {
-  interestFields?: string | null;
-}
-
-export async function readItemCollection({
-  interestFields = null,
-}: readItemCollectionProps): Promise<IItem[]> {
-  // Declare variable for later use
-  let objectsFromItemCollection: IItem[] = [];
-
-  // Connect to MongoDB with mongoose
+export async function getItemById(id: number): Promise<IShortItem> {
   try {
-    // Start connection
-    await connect(mongoConfig.MONGO_URI, mongoConfig.MONGO_CONNECT_OPTIONS);
-    console.log('Db_read: Connected to MongoDB.');
+    // Try to find item
+    const item = await prisma.item.findUnique({
+      where: { id },
+      select: {
+        name: true,
+        model: true,
+        brand: true,
+        categories: {
+          select: {
+            category: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+        description: true,
+        stores: {
+          select: {
+            store: {
+              select: {
+                name: true,
+                logo: true,
+              },
+            },
+            url: true,
+          },
+        },
+        isActive: true,
+        searchCount: true,
+      },
+    });
 
-    // Find all objects inside Item collections that are active
-    // TODO: .find() Errors are unhandled here !!!
-    objectsFromItemCollection = await Item.find(
-      { isActive: true },
-      interestFields
-    );
-  } catch (err) {
-    // Error handling
-    console.log(`Db_read: Error connecting to the database: ${err}`);
-  } finally {
-    await connection.close();
-    console.log('Db_read: Mongoose closed.');
+    console.log(item);
+
+    // If item was not found
+    if (!item) {
+      throw new Error('Not found');
+    }
+
+    // If item was found and isActive = false
+    if (!item.isActive) {
+      throw new Error('Forbidden');
+    }
+
+    // If item was found and isActive = true
+    await prisma.item.update({
+      where: { id },
+      data: { searchCount: { increment: 1 } },
+    });
+
+    // Transform data
+    const result: IShortItem = {
+      name: item.name,
+      model: item.model,
+      brand: item.brand,
+      categories: item.categories.map((c) => ({ name: c.category.name })),
+      description: item.description as Prisma.JsonObject,
+      stores: item.stores.map((s) => ({
+        name: s.store.name,
+        logo: s.store.logo,
+        url: s.url,
+      })),
+    };
+
+    return result;
+  } catch (error) {
+    // Throw error to whoever called this
+    // console.error(`Error fetching item ID=${id}:`, error);
+    throw error;
   }
-
-  return objectsFromItemCollection;
 }
-
-// Define interface for props used in readHistoryCollection function
-interface readHistoryCollectionProps {
-  searchParams: FilterQuery<IHistory>;
-  interestFields?: String | null;
-}
-
-export async function readHistoryCollection({
-  searchParams,
-  interestFields = null,
-}: readHistoryCollectionProps): Promise<IHistory[]> {
-  // Declare variable for later use
-  let objectsFromHistoryCollection: IHistory[] = [];
-
-  // Connect to MongoDB with mongoose
-  try {
-    // Start connection
-    await connect(mongoConfig.MONGO_URI, mongoConfig.MONGO_CONNECT_OPTIONS);
-    console.log('Db_read: Connected to MongoDB.');
-
-    // Find all objects inside Item collections that are active
-    objectsFromHistoryCollection = await History.find(
-      searchParams,
-      interestFields
-    );
-    // .find() Errors are unhandled here !!!
-  } catch (err) {
-    // Error handling
-    console.log(`Db_read: Error connecting to the database: ${err}`);
-  } finally {
-    await connection.close();
-    console.log('Db_read: Mongoose closed.');
-  }
-
-  // console.log(objectsFromHistoryCollection);
-
-  return objectsFromHistoryCollection;
-}
-
-async function testItemRead() {
-  const test = await readItemCollection({
-    interestFields: '_id stores isActive lowestPrice',
-  });
-  console.log(test);
-}
-
-async function testHistoryRead() {
-  const listOfIds: Types.ObjectId[] = [
-    new Types.ObjectId('67227ed8a47e7d929cd3d214'),
-    new Types.ObjectId('67227ed8a47e7d929cd3d213'),
-  ];
-
-  const test = await readHistoryCollection({
-    searchParams: {
-      item_id: { $in: listOfIds },
-    },
-  });
-  console.log(test);
-}
-
-// testItemRead();
-// testHistoryRead();

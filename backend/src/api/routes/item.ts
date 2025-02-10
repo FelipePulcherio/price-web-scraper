11a10d626d82f82e4c390e0517b392fba3675322
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 18:44:42 -0800

    Add GET /api/v1/items/history/30/:itemId

--- backend/src/api/routes/item.ts
+++ backend/src/api/routes/item.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import {
   getItemById,
   getItemsByCategoryId,
+  getLowestPricesByItemId,
 } from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 
@@ -73,6 +74,44 @@ export default (app: Router) => {
         error instanceof Error ? error.message : 'Uknown error';
 
       console.error('GET /api/v1/items/category/:categoryId', errorMessage);
+
+      // Not found
+      if (errorMessage.includes('Not found')) {
+        res.status(404).json(resFormatter(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res
+          .status(500)
+          .json(resFormatter(false, 'Internal server error', null));
+      }
+    }
+  });
+
+  // GET /api/v1/items/history/30/:itemId
+  // Used to get last 30 days prices of a specific item
+  route.get('/history/30/:itemId', async (req: Request, res: Response) => {
+    try {
+      const itemId = parseInt(req.params.itemId, 10);
+      console.log(`GET /api/v1/items/history/30/${itemId}`);
+
+      const fetchedPrices = await getLowestPricesByItemId(itemId, 30);
+      // console.log(fetchedItem);
+
+      res
+        .status(200)
+        .json(
+          resFormatter(
+            true,
+            'Lowest prices fetched successfully',
+            fetchedPrices
+          )
+        );
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('GET /api/v1/items/:id', errorMessage);
 
       // Not found
       if (errorMessage.includes('Not found')) {

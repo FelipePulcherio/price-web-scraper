8e9c217f8cc5120faad564291078993fbb72d193
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 17:58:19 -0800

    Add GET /api/v1/search/quick?q=

---
+++ backend/src/api/routes/search.ts
@@ -0,0 +1,42 @@
+import { Router, Request, Response } from 'express';
+import { searchItemQuick } from '@/database/operations/dbRead';
+import resFormatter from '@/helpers/apiResponseFormatter';
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use('/search', route);
+
+  // GET /api/v1/search/quick?q=
+  // Used to find up to 5 items from a search
+  route.get('/quick', async (req: Request, res: Response) => {
+    try {
+      const search = req.query.q as string;
+
+      console.log(`GET /api/v1/search/quick?q=${search}`);
+
+      const fetchedItems = await searchItemQuick(search);
+      // console.log(fetchedItems);
+
+      res
+        .status(200)
+        .json(resFormatter(true, 'Items fetched successfully', fetchedItems));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('GET /api/v1/search/quick?q=', errorMessage);
+
+      // Not found
+      if (errorMessage.includes('Not found')) {
+        res.status(404).json(resFormatter(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res
+          .status(500)
+          .json(resFormatter(false, 'Internal server error', null));
+      }
+    }
+  });
+};

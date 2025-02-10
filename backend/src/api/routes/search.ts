fdd0d65d5ab2f7ab93eeb4a1d52f006435d30d9d
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 18:09:01 -0800

    Add GET /api/v1/search?q=

--- backend/src/api/routes/search.ts
+++ backend/src/api/routes/search.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { searchItemQuick } from '@/database/operations/dbRead';
+import { searchItemByString } from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 
 const route = Router();
@@ -15,7 +15,7 @@ export default (app: Router) => {
 
       console.log(`GET /api/v1/search/quick?q=${search}`);
 
-      const fetchedItems = await searchItemQuick(search);
+      const fetchedItems = await searchItemByString(search, 5, 1);
       // console.log(fetchedItems);
 
       res
@@ -26,6 +26,41 @@ export default (app: Router) => {
         error instanceof Error ? error.message : 'Uknown error';
 
       console.error('GET /api/v1/search/quick?q=', errorMessage);
+
+      // Not found
+      if (errorMessage.includes('Not found')) {
+        res.status(404).json(resFormatter(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res
+          .status(500)
+          .json(resFormatter(false, 'Internal server error', null));
+      }
+    }
+  });
+
+  // GET /api/v1/search/quick?q=
+  // Used on regular searches. Use pages with 24 items
+  route.get('/', async (req: Request, res: Response) => {
+    try {
+      const search = req.query.q as string;
+      const pageSize = 24;
+      const page = parseInt(req.query.page as string, 10) || 1;
+
+      console.log(`GET /api/v1/search?q=${search}`);
+
+      const fetchedItems = await searchItemByString(search, pageSize, page);
+      // console.log(fetchedItems);
+
+      res
+        .status(200)
+        .json(resFormatter(true, 'Items fetched successfully', fetchedItems));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('GET /api/v1/search?q=', errorMessage);
 
       // Not found
       if (errorMessage.includes('Not found')) {

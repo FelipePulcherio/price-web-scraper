d5ab666a756b2292f4e1e14dcc03cd52b80daf8b
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 17:56:08 -0800

    Remove GET /api/v1/items?search=

--- backend/src/api/routes/item.ts
+++ backend/src/api/routes/item.ts
@@ -47,39 +47,6 @@ export default (app: Router) => {
     }
   });
 
-  // GET /api/v1/items?search=
-  // Used to find up to 5 items from a search
-  route.get('/', async (req: Request, res: Response) => {
-    try {
-      const search = req.query.search as string;
-
-      console.log(`GET /api/v1/items?search=${search}`);
-
-      const fetchedItems = await searchItemQuick(search);
-      // console.log(fetchedItems);
-
-      res
-        .status(200)
-        .json(resFormatter(true, 'Items fetched successfully', fetchedItems));
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/items?search=', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, errorMessage, null));
-      }
-      // All other errors
-      else {
-        res
-          .status(500)
-          .json(resFormatter(false, 'Internal server error', null));
-      }
-    }
-  });
-
   // GET /api/v1/items/category/:categoryId?page=
   // Used to find all items that are related to a categoryId
   route.get('/category/:categoryId', async (req: Request, res: Response) => {

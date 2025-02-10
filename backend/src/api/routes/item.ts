0bdced534d640af25637ba16f71980841c67be7d
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-09 17:41:09 -0800

    Add GET /api/v1/items?search=

--- backend/src/api/routes/item.ts
+++ backend/src/api/routes/item.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import {
   getItemById,
   getItemsByCategoryId,
+  searchItemQuick,
 } from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 
@@ -36,6 +37,39 @@ export default (app: Router) => {
       // Forbidden
       else if (errorMessage.includes('Forbidden')) {
         res.status(403).json(resFormatter(false, errorMessage, null));
+      }
+      // All other errors
+      else {
+        res
+          .status(500)
+          .json(resFormatter(false, 'Internal server error', null));
+      }
+    }
+  });
+
+  // GET /api/v1/items?search=
+  // Used to find up to 5 items from a search
+  route.get('/', async (req: Request, res: Response) => {
+    try {
+      const search = req.query.search as string;
+
+      console.log(`GET /api/v1/items?search=${search}`);
+
+      const fetchedItems = await searchItemQuick(search);
+      // console.log(fetchedItems);
+
+      res
+        .status(200)
+        .json(resFormatter(true, 'Items fetched successfully', fetchedItems));
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'Uknown error';
+
+      console.error('GET /api/v1/items?search=', errorMessage);
+
+      // Not found
+      if (errorMessage.includes('Not found')) {
+        res.status(404).json(resFormatter(false, errorMessage, null));
       }
       // All other errors
       else {

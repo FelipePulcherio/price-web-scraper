dd8d69f05b564991eb2906dd6548a23749fba003
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-11 19:40:04 -0800

    Add function deleteScraperCollection()

--- backend/src/database/operations/dbDelete.ts
+++ backend/src/database/operations/dbDelete.ts
@@ -1,35 +1,39 @@
 import { connect, connection } from 'mongoose';
-import { mongoConfig } from '../../config/config';
+import config from '@/config';
 
 // FUNCTIONS
-
-export async function deleteScraperJobsCollection(): Promise<void> {
+export async function deleteScraperCollection(): Promise<void> {
   // Connect to MongoDB with mongoose
   try {
-    await connect(mongoConfig.MONGO_URI, mongoConfig.MONGO_CONNECT_OPTIONS);
+    await connect(config.mongoUri, {
+      retryWrites: true,
+      w: 'majority',
+    });
     console.log('Db_delete: Connected to MongoDB.');
 
-    const schedulerCollection = await connection.db
-      .listCollections({ name: 'Scheduler-jobs' })
-      .hasNext();
+    if (connection.db) {
+      const scraperCollection = await connection.db
+        .listCollections({ name: 'Scraper' })
+        .hasNext();
 
-    // Check if collection exists
-    if (schedulerCollection) {
-      const deleteResult = await connection.db
-        .collection('Scheduler-jobs')
-        .deleteMany({});
+      // Check if collection exists
+      if (scraperCollection) {
+        const deleteResult = await connection.db
+          .collection('Scraper')
+          .deleteMany({});
 
-      if (deleteResult.deletedCount > 0) {
-        console.log(
-          `Db_delete: Deleted ${deleteResult.deletedCount} documents with name "fetcher" in the "Scheduler-jobs" collection.`
-        );
+        if (deleteResult.deletedCount > 0) {
+          console.log(
+            `Db_delete: Deleted ${deleteResult.deletedCount} documents in the "Scraper" collection.`
+          );
+        } else {
+          console.log(
+            'Db_delete: No documents were found in the "Scraper" collection.'
+          );
+        }
       } else {
-        console.log(
-          'Db_delete: No documents with name "fetcher" were found in the "Scheduler-jobs" collection.'
-        );
+        console.log('Db_delete: "Scraper" collection was not found.');
       }
-    } else {
-      console.log('Db_delete: "Scheduler-jobs" collection was not found.');
     }
   } catch (err) {
     console.error(`Db_delete: Error connecting to the database: ${err}`);

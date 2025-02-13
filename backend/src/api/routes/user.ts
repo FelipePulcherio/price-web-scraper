cfa84c9ae49ecdc34f4f97d7887baa292a5fc820
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-13 13:35:58 -0800

    Add GET /api/v1/users/me

--- backend/src/api/routes/user.ts
+++ backend/src/api/routes/user.ts
@@ -1,7 +1,22 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+import middlewares from '../middlewares';
 
 const route = Router();
 
 export default (app: Router) => {
   app.use('/users', route);
+
+  // GET /api/v1/users/me
+  // Used when user tries to keep connected
+  route.get('/me', async (req: Request, res: Response, next: NextFunction) => {
+    console.log('GET /api/v1/users/me Request Body:');
+    console.log(req.body);
+
+    try {
+    } catch (error) {
+      next(error); // Pass errors to middleware
+    }
+  });
+
+  app.use(middlewares.errorHandler);
 };

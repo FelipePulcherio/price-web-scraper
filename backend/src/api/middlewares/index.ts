e4d43d203595cf54b977e351d27708b545416b78
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-13 15:31:47 -0800

    Add validateSignin and verifyPassword

--- backend/src/api/middlewares/index.ts
+++ backend/src/api/middlewares/index.ts
@@ -1,7 +1,10 @@
 import errorHandler from './errorHandler';
-import { validateSignup } from './validation';
+import { validateSignup, validateSignin } from './validation';
+import verifyPassword from './verifyPassword';
 
 export default {
   errorHandler,
   validateSignup,
+  validateSignin,
+  verifyPassword,
 };

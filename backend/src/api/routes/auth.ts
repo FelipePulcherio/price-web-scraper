4b08d9af51397ef01ab458af20c6d8d4d060a722
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-13 12:11:01 -0800

    Add route GET /api/v1/user/me with middlewares

---
+++ backend/src/api/routes/auth.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import resFormatter from '@/helpers/apiResponseFormatter';
+import middlewares from '../middlewares';
+
+const route = Router();
+
+export default (app: Router) => {
+  app.use('/user', route);
+
+  // GET /api/v1/user/me
+  // Used when user tries to keep connected
+  route.get('/me', async (req: Request, res: Response, next: NextFunction) => {
+    console.log('POST /api/v1/user/me Request Body:');
+    console.log(req.body);
+
+    try {
+    } catch (error) {
+      next(error); // Pass errors to middleware
+    }
+  });
+
+  app.use(middlewares.errorHandler);
+};

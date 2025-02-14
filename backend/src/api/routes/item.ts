580253894d8658ed82a3aeb0736bb864f15c3e71
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-14 10:33:53 -0800

    Use 'next(error)' in all routes

--- backend/src/api/routes/item.ts
+++ backend/src/api/routes/item.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import {
   getItemById,
   getItemsByCategoryId,
@@ -13,7 +13,7 @@ export default (app: Router) => {
 
   // GET /api/v1/items/:id
   // Used to find a specific item
-  route.get('/:id', async (req: Request, res: Response) => {
+  route.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
     try {
       const itemId = parseInt(req.params.id, 10);
       console.log(`GET /api/v1/items/:id Request param: ${itemId}`);
@@ -25,104 +25,69 @@ export default (app: Router) => {
         .status(200)
         .json(resFormatter(true, ['Item fetched successfully'], fetchedItem));
     } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/items/:id', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, [errorMessage], null));
-      }
-      // Forbidden
-      else if (errorMessage.includes('Forbidden')) {
-        res.status(403).json(resFormatter(false, [errorMessage], null));
-      }
-      // All other errors
-      else {
-        res
-          .status(500)
-          .json(resFormatter(false, ['Internal server error'], null));
-      }
+      // Pass errors to middlewares.errorHandler
+      next(error);
     }
   });
 
   // GET /api/v1/items/category/:categoryId?page=
   // Used to find all items that are related to a categoryId
-  route.get('/category/:categoryId', async (req: Request, res: Response) => {
-    try {
-      const categoryId = parseInt(req.params.categoryId, 10);
-      const pageSize = 24;
-      const page = parseInt(req.query.page as string, 10) || 1;
+  route.get(
+    '/category/:categoryId',
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const categoryId = parseInt(req.params.categoryId, 10);
+        const pageSize = 24;
+        const page = parseInt(req.query.page as string, 10) || 1;
 
-      console.log(
-        `GET /api/v1/items/category/:categoryId Request param: ${categoryId}`
-      );
+        console.log(
+          `GET /api/v1/items/category/:categoryId Request param: ${categoryId}`
+        );
 
-      const fetchedItems = await getItemsByCategoryId(
-        categoryId,
-        pageSize,
-        page
-      );
-      // console.log(fetchedItems);
+        const fetchedItems = await getItemsByCategoryId(
+          categoryId,
+          pageSize,
+          page
+        );
+        // console.log(fetchedItems);
 
-      res
-        .status(200)
-        .json(resFormatter(true, ['Items fetched successfully'], fetchedItems));
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/items/category/:categoryId', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, [errorMessage], null));
-      }
-      // All other errors
-      else {
         res
-          .status(500)
-          .json(resFormatter(false, ['Internal server error'], null));
+          .status(200)
+          .json(
+            resFormatter(true, ['Items fetched successfully'], fetchedItems)
+          );
+      } catch (error) {
+        // Pass errors to middlewares.errorHandler
+        next(error);
       }
     }
-  });
+  );
 
   // GET /api/v1/items/history/30/:itemId
   // Used to get last 30 days prices of a specific item
-  route.get('/history/30/:itemId', async (req: Request, res: Response) => {
-    try {
-      const itemId = parseInt(req.params.itemId, 10);
-      console.log(`GET /api/v1/items/history/30/${itemId}`);
+  route.get(
+    '/history/30/:itemId',
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const itemId = parseInt(req.params.itemId, 10);
+        console.log(`GET /api/v1/items/history/30/${itemId}`);
 
-      const fetchedPrices = await getLowestPricesByItemId(itemId, 30);
-      // console.log(fetchedItem);
+        const fetchedPrices = await getLowestPricesByItemId(itemId, 30);
+        // console.log(fetchedItem);
 
-      res
-        .status(200)
-        .json(
-          resFormatter(
-            true,
-            ['Lowest prices fetched successfully'],
-            fetchedPrices
-          )
-        );
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/items/:id', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, [errorMessage], null));
-      }
-      // All other errors
-      else {
         res
-          .status(500)
-          .json(resFormatter(false, ['Internal server error'], null));
+          .status(200)
+          .json(
+            resFormatter(
+              true,
+              ['Lowest prices fetched successfully'],
+              fetchedPrices
+            )
+          );
+      } catch (error) {
+        // Pass errors to middlewares.errorHandler
+        next(error);
       }
     }
-  });
+  );
 };

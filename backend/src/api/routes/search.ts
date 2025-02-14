549884cbd1ebd0ec6a72a1d50f3d586765502015
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-14 10:53:34 -0800

    Pass errors to middlewares.errorHandler

--- backend/src/api/routes/search.ts
+++ backend/src/api/routes/search.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { searchItemByString } from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 
@@ -9,40 +9,32 @@ export default (app: Router) => {
 
   // GET /api/v1/search/quick?q=
   // Used to find up to 5 items from a search
-  route.get('/quick', async (req: Request, res: Response) => {
-    try {
-      const search = req.query.q as string;
+  route.get(
+    '/quick',
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const search = req.query.q as string;
 
-      console.log(`GET /api/v1/search/quick?q=${search}`);
+        console.log(`GET /api/v1/search/quick?q=${search}`);
 
-      const fetchedItems = await searchItemByString(search, 5, 1);
-      // console.log(fetchedItems);
+        const fetchedItems = await searchItemByString(search, 5, 1);
+        // console.log(fetchedItems);
 
-      res
-        .status(200)
-        .json(resFormatter(true, ['Items fetched successfully'], fetchedItems));
-    } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/search/quick?q=', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, [errorMessage], null));
-      }
-      // All other errors
-      else {
         res
-          .status(500)
-          .json(resFormatter(false, ['Internal server error'], null));
+          .status(200)
+          .json(
+            resFormatter(true, ['Items fetched successfully'], fetchedItems)
+          );
+      } catch (error) {
+        // Pass errors to middlewares.errorHandler
+        next(error);
       }
     }
-  });
+  );
 
   // GET /api/v1/search?q=
   // Used on regular searches. Use pages with 24 items
-  route.get('/', async (req: Request, res: Response) => {
+  route.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
       const search = req.query.q as string;
       const pageSize = 24;
@@ -57,21 +49,8 @@ export default (app: Router) => {
         .status(200)
         .json(resFormatter(true, ['Items fetched successfully'], fetchedItems));
     } catch (error) {
-      const errorMessage =
-        error instanceof Error ? error.message : 'Uknown error';
-
-      console.error('GET /api/v1/search?q=', errorMessage);
-
-      // Not found
-      if (errorMessage.includes('Not found')) {
-        res.status(404).json(resFormatter(false, [errorMessage], null));
-      }
-      // All other errors
-      else {
-        res
-          .status(500)
-          .json(resFormatter(false, ['Internal server error'], null));
-      }
+      // Pass errors to middlewares.errorHandler
+      next(error);
     }
   });
 };

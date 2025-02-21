f237ea7cc2330d1aba6406cb9e10b1c08ce83ce9
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-20 16:48:22 -0800

    Add more routes

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,12 +1,37 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import { paths } from '@/config/paths';
 import LandingRoute from './routes/landing';
+import AllCategoriesRoute from './routes/app/allCategories';
+import SearchRoute from './routes/app/search';
+import ItemRoute from './routes/app/item';
+import LoginRoute from './routes/auth/login';
+import RegisterRoute from './routes/auth/register';
 
 export const createAppRouter = () =>
   createBrowserRouter([
     {
       path: paths.home.path,
       element: <LandingRoute />,
+    },
+    {
+      path: paths.app.allCategories.path,
+      element: <AllCategoriesRoute />,
+    },
+    {
+      path: paths.app.search.path,
+      element: <SearchRoute />,
+    },
+    {
+      path: paths.app.item.path,
+      element: <ItemRoute />,
+    },
+    {
+      path: paths.auth.login.path,
+      element: <LoginRoute />,
+    },
+    {
+      path: paths.auth.register.path,
+      element: <RegisterRoute />,
     },
   ]);
 
--- frontend/src/config/paths.ts
+++ frontend/src/config/paths.ts
@@ -3,4 +3,37 @@ export const paths = {
     path: '/',
     getHref: () => '/',
   },
+
+  auth: {
+    register: {
+      path: '/auth/register',
+      getHref: (redirectTo?: string | null | undefined) =>
+        `/auth/register${
+          redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''
+        }`,
+    },
+    login: {
+      path: '/auth/login',
+      getHref: (redirectTo?: string | null | undefined) =>
+        `/auth/login${
+          redirectTo ? `?redirectTo=${encodeURIComponent(redirectTo)}` : ''
+        }`,
+    },
+  },
+
+  app: {
+    allCategories: {
+      path: '/all-categories',
+      getHref: () => '/all-categories',
+    },
+    search: {
+      path: '/search',
+      getHref: (query: string) => `/search?q=${encodeURIComponent(query)}`,
+    },
+    item: {
+      path: '/item/:brand/:model',
+      getHref: (brand: string, model: string) =>
+        `/item/${encodeURIComponent(brand)}/${encodeURIComponent(model)}`,
+    },
+  },
 } as const;

05c55e63a1d7e92a4ec981d51d0079ffe156c5bc
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-21 11:03:52 -0800

    Add cors

--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import config from '@/config';
 import routes from '@/api';
 
 // Code from: https://github.com/santiq/bulletproof-nodejs/
 export default ({ app }: { app: express.Application }) => {
+  // Use cors for integration with frontend
+  const corsOptions = {
+    origin: ['http://localhost:5173'],
+  };
+
+  app.use(cors(corsOptions));
+
   // Transforms the raw string of req.body into json
   app.use(express.json());
 

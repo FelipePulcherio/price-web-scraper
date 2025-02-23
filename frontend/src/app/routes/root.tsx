77ad15670ba68ea97f40745a62dd34b2ebafe526
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-23 07:40:44 -0800

    Create AppRoot

--- frontend/src/app/routes/root.tsx
+++ frontend/src/app/routes/root.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from 'react-router';
+import { AppLayout } from '@/components/layouts';
+
+export const ErrorBoundary = () => {
+  return <div>Something went wrong!</div>;
+};
 
 const AppRoot = () => {
-  return <Outlet />;
+  return (
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
+  );
 };
 
 export default AppRoot;

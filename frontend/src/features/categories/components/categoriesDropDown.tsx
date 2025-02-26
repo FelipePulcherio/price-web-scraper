16e85b144a29fb19aba87e9f2b718e01920cf438
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-25 18:33:37 -0800

    Remove unused import

--- frontend/src/features/categories/components/categoriesDropDown.tsx
+++ frontend/src/features/categories/components/categoriesDropDown.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ChevronDown, CircleChevronUp } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,

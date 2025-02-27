40453b796a681c2952cb6278758ebb0ca3653fb5
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-02-27 15:57:09 -0800

    Change breakpoints

--- frontend/src/features/categories/components/categories.tsx
+++ frontend/src/features/categories/components/categories.tsx
@@ -3,15 +3,13 @@ import { CategoriesSheet } from './categoriesSheet';
 
 export const Categories = () => {
   return (
-    <div>
-      <div className='flex items-center justify-center'>
-        <div className='hidden md:block'>
-          <CategoriesDropDown />
-        </div>
-        <div className='md:hidden'>
-          <CategoriesSheet />
-        </div>
+    <>
+      <div className='hidden sm:block'>
+        <CategoriesDropDown />
       </div>
-    </div>
+      <div className='sm:hidden'>
+        <CategoriesSheet />
+      </div>
+    </>
   );
 };

9090200cdc8c396ff0fad3ea25ccdff7f443f7dc
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-01 15:24:12 -0800

    Add isAuth and attachCurrentUser

--- backend/src/api/middlewares/index.ts
+++ backend/src/api/middlewares/index.ts
@@ -1,6 +1,8 @@
 import errorHandler from './errorHandler';
 import { validateSignup, validateSignin, validateSearch } from './validation';
 import verifyPassword from './verifyPassword';
+import isAuth from './isAuth';
+import attachCurrentUser from './attachCurrentUser';
 
 export default {
   errorHandler,
@@ -8,4 +10,6 @@ export default {
   validateSignin,
   validateSearch,
   verifyPassword,
+  isAuth,
+  attachCurrentUser,
 };

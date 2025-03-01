37eafffd79f28438780fb52a9b85248260afe2f1
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-01 08:28:51 -0800

    Add component

---
+++ frontend/src/features/ads/components/adsCard.tsx
@@ -0,0 +1,7 @@
+import { Card } from '@/components/ui/card';
+
+export function AdsCard(): React.ReactNode {
+  return (
+    <Card className='w-full h-45 bg-gradient-to-br from-purple-300 to-cyan-300'></Card>
+  );
+}

ba24db539892ca0be0d1677d19799d2584bb3b0d
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-01 15:29:24 -0800

    Add GET /api/v1/users/me

--- backend/src/api/routes/user.ts
+++ backend/src/api/routes/user.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import middlewares from '../middlewares';
+import resFormatter from '@/helpers/apiResponseFormatter';
 
 const route = Router();
 
@@ -8,15 +9,26 @@ export default (app: Router) => {
 
   // GET /api/v1/users/me
   // Used when user tries to keep connected
-  route.get('/me', async (req: Request, res: Response, next: NextFunction) => {
-    console.log('GET /api/v1/users/me Request Body:');
-    console.log(req.body);
+  route.get(
+    '/me',
+    middlewares.isAuth,
+    middlewares.attachCurrentUser,
+    async (req: Request, res: Response, next: NextFunction) => {
+      console.log('GET /api/v1/users/me Request Body:');
+      // console.log(req.body);
 
-    try {
-    } catch (error) {
-      next(error); // Pass errors to middleware
+      res
+        .status(200)
+        .json(
+          resFormatter(true, ['Stores fetched successfully'], {
+            user: req.currentUser,
+          })
+        );
+
+      try {
+      } catch (error) {
+        next(error); // Pass errors to middleware
+      }
     }
-  });
-
-  app.use(middlewares.errorHandler);
+  );
 };

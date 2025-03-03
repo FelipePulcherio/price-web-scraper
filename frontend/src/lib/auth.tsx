99847fe0828c784d0ff8472e50027ab1947794de
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 15:17:37 -0800

    Add zod schemas + auth apis + queryAuth configs

---
+++ frontend/src/lib/auth.tsx
@@ -0,0 +1,113 @@
+import { z } from 'zod';
+import { configureAuth } from 'react-query-auth';
+import { Navigate, useLocation } from 'react-router';
+
+import { paths } from '@/config/paths';
+
+import { api } from './apiClient';
+import { IAPI } from '@/types/interfaces';
+
+// APIs, Schemas and types
+// Login
+const getUser = async (): Promise<IAPI> => {
+  const response = await api.get('/users/me');
+
+  return response.data.user;
+};
+
+const logout = (): Promise<IAPI> => {
+  return api.post('/auth/logout');
+};
+
+export const loginInputSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.',
+  }),
+});
+
+export type LoginInput = z.infer<typeof loginInputSchema>;
+
+const loginWithEmailAndPassword = (data: LoginInput): Promise<IAPI> => {
+  return api.post('/auth/signin', data);
+};
+
+// Register
+export const registerInputSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, {
+      message: 'First name must be at least 2 characters.',
+    })
+    .max(50),
+  lastName: z
+    .string()
+    .min(2, {
+      message: 'Last name must be at least 2 characters.',
+    })
+    .max(50),
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  phone: z
+    .string()
+    .regex(/^[0-9]*$/, {
+      message: 'Please use only numbers.',
+    })
+    .min(10, {
+      message: 'Phone number must be at least 10 characters.',
+    })
+    .max(15),
+  password: z
+    .string()
+    .min(8, {
+      message: 'Password must be at least 8 characters.',
+    })
+    .regex(
+      // eslint-disable-next-line no-useless-escape
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]).+$/,
+      {
+        message:
+          'Password must contain at least one uppercase letter, one lowercase letter, one number and one symbol.',
+      }
+    ),
+});
+
+export type RegisterInput = z.infer<typeof registerInputSchema>;
+
+const registerWithEmailAndPassword = (data: RegisterInput): Promise<IAPI> => {
+  return api.post('/auth/signup', data);
+};
+
+// Routes
+
+const authConfig = {
+  userFn: getUser,
+  loginFn: async (data: LoginInput) => {
+    const response = await loginWithEmailAndPassword(data);
+    return response;
+  },
+  registerFn: async (data: RegisterInput) => {
+    const response = await registerWithEmailAndPassword(data);
+    return response;
+  },
+  logoutFn: logout,
+};
+
+export const { useUser, useLogin, useLogout, useRegister, AuthLoader } =
+  configureAuth(authConfig);
+
+export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const user = useUser();
+  const location = useLocation();
+
+  if (!user.data) {
+    return (
+      <Navigate to={paths.auth.login.getHref(location.pathname)} replace />
+    );
+  }
+
+  return children;
+};

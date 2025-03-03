b4f5c8dfbc98d5a20ea87486f3c1ac797cb83b19
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 15:11:03 -0800

    Add and use AuthLayout

--- frontend/src/app/routes/auth/root.tsx
+++ frontend/src/app/routes/auth/root.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router';
+import { AuthLayout } from '@/components/layouts';
 
 export const ErrorBoundary = () => {
   return <div>Something went wrong in auth pages!</div>;
 };
 
 const AuthRoot = () => {
-  return <Outlet />;
+  return (
+    <AuthLayout>
+      <Outlet />
+    </AuthLayout>
+  );
 };
 
 export default AuthRoot;

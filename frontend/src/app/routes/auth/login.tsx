77d0e58a7b33db32caa5419c8c7f6361049cdf5b
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 15:13:38 -0800

    Use LoginForm in LoginRoute

--- frontend/src/app/routes/auth/login.tsx
+++ frontend/src/app/routes/auth/login.tsx
@@ -1,8 +1,26 @@
+import { useNavigate, useSearchParams } from 'react-router';
+
+import { paths } from '@/config/paths';
+import { LoginForm } from '@/features/auth/components/loginForm';
+import { toast } from 'sonner';
+
 const LoginRoute = () => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get('redirectTo');
+
   return (
-    <div className='container'>
-      <div>Login</div>
-    </div>
+    <LoginForm
+      onSuccess={() => {
+        toast.success('Sign in successful', {
+          description: 'You have been signed in to your account.',
+        });
+
+        navigate(`${redirectTo ? `${redirectTo}` : paths.root.getHref()}`, {
+          replace: true,
+        });
+      }}
+    />
   );
 };
 

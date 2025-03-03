b08e9f0741b0ab1c776e93ac956fad28288cb6e2
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 15:01:18 -0800

    Add cookie-parser

--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import config from '@/config';
 import routes from '@/api';
 
@@ -8,12 +9,16 @@ export default ({ app }: { app: express.Application }) => {
   // Use cors for integration with frontend
   const corsOptions = {
     origin: ['http://localhost:5173'],
+    credentials: true,
   };
 
   app.use(cors(corsOptions));
 
   // Transforms the raw string of req.body into json
   app.use(express.json());
+
+  // Cookie parser
+  app.use(cookieParser());
 
   // Load API routes
   app.use(config.api.prefix, routes());

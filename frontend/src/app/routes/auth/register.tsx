fb533a4d5d853fc1fd809599aca9490ded786446
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 18:06:46 -0800

    Import and use RegisterForm

--- frontend/src/app/routes/auth/register.tsx
+++ frontend/src/app/routes/auth/register.tsx
@@ -1,9 +1,25 @@
-const RegisterRoute = () => {
-  return (
-    <div className='container'>
-      <div>Register</div>
-    </div>
-  );
-};
+import { useNavigate, useSearchParams } from 'react-router';
 
-export default RegisterRoute;
+import { paths } from '@/config/paths';
+import { RegisterForm } from '@/features/auth/components/registerForm';
+import { toast } from 'sonner';
+
+export default function RegisterRoute() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get('redirectTo');
+
+  return (
+    <RegisterForm
+      onSuccess={() => {
+        toast.success('Register successful', {
+          description: 'You have been signed in to your account.',
+        });
+
+        navigate(`${redirectTo ? `${redirectTo}` : paths.root.getHref()}`, {
+          replace: true,
+        });
+      }}
+    />
+  );
+}

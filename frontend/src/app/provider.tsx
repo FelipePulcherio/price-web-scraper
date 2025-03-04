34c66d46c60094f3f27d47daf90c15b23cd9ed8f
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 18:01:59 -0800

    Add react-query and it's provider

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,9 +1,33 @@
-import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState, ReactNode } from 'react';
+import { queryConfig } from '@/lib/reactQuery';
+import { AuthLoader } from '@/lib/auth';
+import { ErrorBoundary } from 'react-error-boundary';
+import { MainErrorFallback } from '@/components/errors/main';
 
 type AppProviderProps = {
   children: ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  return <>{children}</>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      })
+  );
+
+  return (
+    <ErrorBoundary FallbackComponent={MainErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <AuthLoader
+          renderLoading={() => (
+            <div className='size-screen flex items-center justify-center'></div>
+          )}
+        >
+          {children}
+        </AuthLoader>
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
 };

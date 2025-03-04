586bf431cd7d7065a49b52a96d4106b47a3b2313
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-03 18:22:41 -0800

    Add POST /api/v1/auth/logout route

--- backend/src/api/routes/auth.ts
+++ backend/src/api/routes/auth.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 import config from '@/config';
 import { createUser } from '@/database/operations/dbCreate';
 import resFormatter from '@/helpers/apiResponseFormatter';
-import { IUser } from '@/interfaces/interfaces';
+import { IAuthUser, IUser } from '@/interfaces/interfaces';
 
 const route = Router();
 
@@ -38,33 +38,58 @@ export default (app: Router) => {
     async (req: Request, res: Response, next: NextFunction) => {
       console.log('POST /api/v1/auth/signin Request Body:', req.body);
       try {
-        const user = req.user as IUser;
+        // Make sure currentUser was attached
+        if (!req.currentUser) {
+          return next(new Error('Internal server error'));
+        }
 
         // Create token
         const token = jwt.sign(
           {
-            id: user.id,
-            name: user.firstName,
-            role: user.role,
+            id: req.currentUser.id,
+            firstName: req.currentUser.firstName,
+            email: req.currentUser.email,
+            role: req.currentUser.role,
           },
           config.jwt.secret,
-          { expiresIn: config.jwt.maxAge }
+          { expiresIn: config.jwt.maxAge } // This is in s
         );
 
         // Attach token to cookie
         res.cookie('jwt', token, {
           httpOnly: true,
-          maxAge: config.jwt.maxAge * 1000,
+          maxAge: config.jwt.maxAge * 1000, // This is in ms
         });
 
         res.status(200).json(
           resFormatter(true, ['User logged in successfully'], {
-            user: user.id,
+            user: req.currentUser,
           })
         );
       } catch (error) {
         // Pass errors to middlewares.errorHandler
         next(error);
+      }
+    }
+  );
+
+  // POST /api/v1/auth/logout
+  route.post(
+    '/logout',
+    middlewares.isAuth,
+    middlewares.detachCurrentUser,
+    (req: Request, res: Response, next: NextFunction) => {
+      console.log('POST /api/v1/auth/logout Request Body:', req.body);
+
+      try {
+        res.status(200).json(
+          resFormatter(true, ['User logged out successfully'], {
+            user: req.currentUser,
+          })
+        );
+      } catch (error) {
+        // Pass errors to middlewares.errorHandler
+        return next(error);
       }
     }
   );

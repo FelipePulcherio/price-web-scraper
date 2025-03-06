1da7b95e5da788574d1693691ad212105c477c76
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-05 22:30:08 -0800

    Add navigation

--- frontend/src/features/items/components/itemCard.tsx
+++ frontend/src/features/items/components/itemCard.tsx
@@ -1,5 +1,8 @@
+import { NavLink } from 'react-router';
+import { paths } from '@/config/paths';
 import { Card, CardContent } from '@/components/ui/card';
 import { IShortItem } from '@/types/interfaces';
+import { PriceFormat } from '@/utils/priceFormat';
 
 export function ItemCard({
   id,
@@ -11,27 +14,26 @@ export function ItemCard({
   storesQty,
 }: IShortItem): React.ReactNode {
   return (
-    <Card key={id} className='p-4 h-full'>
-      <CardContent className='p-0 aspect-square flex flex-col justify-start items-center'>
-        <img
-          className='w-full max-w-[250px] aspect-square'
-          src={image.url}
-          alt={`Image ${brand} ${name} ${model}`}
-        />
-        <div className='h-16 w-full pb-2 text-xs'>
-          {`${brand} ${name} - ${model}`}
-          <span className='text-2xs'></span>
-        </div>
-        <div className='w-full pb-2 text-lg font-bold'>
-          {Intl.NumberFormat('en-CA', {
-            style: 'currency',
-            currency: 'CAD',
-          }).format(price as number)}
-        </div>
-        <div className='w-full pb-6 text-zinc-800 text-xs'>{`Compare from ${storesQty} ${
-          storesQty === 1 ? 'store' : 'stores'
-        }`}</div>
-      </CardContent>
-    </Card>
+    <NavLink key={name} to={paths.item.getHref(id, brand, model)} caseSensitive>
+      <Card key={id} className='p-4 h-full'>
+        <CardContent className='p-0 aspect-square flex flex-col justify-start items-center'>
+          <img
+            className='w-full max-w-[250px] aspect-square'
+            src={image.url}
+            alt={`Image ${brand} ${name} ${model}`}
+          />
+          <div className='h-16 w-full pb-2 text-xs'>
+            {`${brand} ${name} - ${model}`}
+            <span className='text-2xs'></span>
+          </div>
+          <div className='w-full pb-2 text-lg font-bold'>
+            {PriceFormat(price as number, 'en-CA', 'CAD')}
+          </div>
+          <div className='w-full pb-6 text-zinc-800 text-xs'>{`Compare from ${storesQty} ${
+            storesQty === 1 ? 'store' : 'stores'
+          }`}</div>
+        </CardContent>
+      </Card>
+    </NavLink>
   );
 }

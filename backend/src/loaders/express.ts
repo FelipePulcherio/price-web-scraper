2f319f71cef1a8db18ff03f733304acc3d85af12
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-03-10 19:21:28 -0700

    deploy fix

--- backend/src/loaders/express.ts
+++ backend/src/loaders/express.ts
@@ -6,13 +6,21 @@ import routes from '@/api';
 
 // Code from: https://github.com/santiq/bulletproof-nodejs/
 export default ({ app }: { app: express.Application }) => {
+  // Health check
+  app.get('/status', (req, res) => {
+    res.status(200).end();
+  });
+  app.head('/status', (req, res) => {
+    res.status(200).end();
+  });
+
   // Use cors for integration with frontend
   const corsOptions = {
     origin: ['https://price-web-scraper-frontend.vercel.app'],
     credentials: true,
   };
 
-  app.use(cors(corsOptions));
+  app.use(cors());
 
   // Transforms the raw string of req.body into json
   app.use(express.json());

fc47b5f51b553df8af9783a66badd2d58aa30500
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-04-13 16:32:46 -0700

    Create func to get auth + adjust output of main func

--- backend/src/services/stores/costcoCa/costcoCaSearch.ts
+++ backend/src/services/stores/costcoCa/costcoCaSearch.ts
@@ -1,19 +1,24 @@
 import { CookieJar } from 'tough-cookie';
 import { axiosClient } from '../storesApiClient';
+import { ICostcoAuthAPI, ICostcoSearchAPI } from './types';
+import { IDiscoverItem } from '@/interfaces/interfaces';
+import formatQuery from '../utils/formatQuery';
+import costcoCaNormalizeData from './costcoCaNormalizeData';
 
-// Cookie jar per store
 const costcoCaJar = new CookieJar();
+let apiKey: string = '';
 
-export default async function costcoCaSearchByQuery() {
+async function getAuth(): Promise<string | null> {
   try {
     // Retrieve cookies from main site: https://www.costco.ca/
     const url = 'https://www.costco.ca/s?keyword=tv%20C65';
-    const initialResponse = await axiosClient.get(url, {
+    const initialResponse: ICostcoAuthAPI = await axiosClient.get(url, {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
         Accept: '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
+        Connection: 'keep-alive',
       },
     });
 
@@ -30,28 +35,63 @@ export default async function costcoCaSearchByQuery() {
     const apiKeyMatch = initialResponse.data.match(
       /{\\\"key\\\":\\\"apikey\\\",\\\"value\\\":\\\"(.*?)\\\"}/
     );
-    if (!apiKeyMatch) throw new Error('API key not found');
+
+    if (!apiKeyMatch) {
+      throw new Error('API key not found.');
+    }
 
     const apiKey = apiKeyMatch[1];
 
     // console.log('Extracted API Key:', apiKey);
 
-    const cookieHeader = await costcoCaJar.getCookieString(
-      'https://www.costco.ca'
-    );
+    return apiKey;
+  } catch (err) {
+    console.error('Error getting Costco auth:', err);
+    return null;
+  }
+}
 
-    // console.log('Cookies:', cookieHeader);
+export default async function costcoCaSearch({
+  checkAuth,
+  query,
+}: {
+  checkAuth: boolean;
+  query: string;
+}): Promise<IDiscoverItem[] | null> {
+  if (checkAuth) {
+    const newApiKey = await getAuth();
 
+    if (!newApiKey) {
+      return null;
+    }
+
+    apiKey = newApiKey;
+  }
+
+  const cookieHeader = await costcoCaJar.getCookieString(
+    'https://www.costco.ca'
+  );
+
+  // console.log('Cookies:', cookieHeader);
+
+  try {
     // Actual api for searching an item
     // Needs Headers, Cookies and X-Api-Key
     // Works by string (with + separator) and id (refer to "item_number")
-    const apiUrl =
-      'https://search.costco.ca/api/apps/www_costco_ca/query/www_costco_ca_search?expoption=def&q=tv+lg+65&locale=en-CA&start=0&expand=false&loc=656-bd%2C548-wh%2C559-wm%2C792-dz%2C792-wm%2C894_0-cwt%2C894_0-edi%2C894_0-membership%2C894_0-mpt%2C894_0-otw%2C894_0-spc%2C894_1-edi%2C894_1-mpt%2C946-wm%2C9894-wcs%2C993-dz%2C993-wm&whloc=548-wh';
+    const apiUrlBase =
+      'https://search.costco.ca/api/apps/www_costco_ca/query/www_costco_ca_search?expoption=def&q=query+here&locale=en-CA&start=0&expand=false&loc=656-bd%2C548-wh%2C559-wm%2C792-dz%2C792-wm%2C894_0-cwt%2C894_0-edi%2C894_0-membership%2C894_0-mpt%2C894_0-otw%2C894_0-spc%2C894_1-edi%2C894_1-mpt%2C946-wm%2C9894-wcs%2C993-dz%2C993-wm&whloc=548-wh&fq=%7B!tag%3Ditem_program_eligibility%7Ditem_program_eligibility%3A(%22ShipIt%22)';
 
-    const apiResponse = await axiosClient.get(apiUrl, {
+    const formattedQuery = formatQuery({ query, separator: '+' });
+
+    const apiUrl = apiUrlBase.replace('query+here', formattedQuery);
+
+    const apiResponse: ICostcoSearchAPI = await axiosClient.get(apiUrl, {
       method: 'GET',
       headers: {
         Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.9',
+        Connection: 'keep-alive',
         Origin: 'https://www.costco.ca',
         Referer: 'https://www.costco.ca/',
         'User-Agent':
@@ -61,10 +101,16 @@ export default async function costcoCaSearchByQuery() {
       },
     });
 
-    console.log('Costco API Response:', apiResponse.data);
+    const discoveredItems: IDiscoverItem[] = costcoCaNormalizeData({
+      apiResponse,
+    });
 
-    return apiResponse.data;
+    console.log('Costco API Response:');
+    console.dir(discoveredItems, { depth: null });
+
+    return discoveredItems;
   } catch (err) {
     console.error('Error fetching Costco data:', err);
+    return null;
   }
 }

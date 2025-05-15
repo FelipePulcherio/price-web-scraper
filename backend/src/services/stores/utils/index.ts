ee7c632e7499edf147720a6e9b146366d87b4efb
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-05-14 19:24:26 -0700

    Add global export for utils module

---
+++ backend/src/services/stores/utils/index.ts
@@ -0,0 +1,21 @@
+import formatQuery from './formatQuery';
+import randomizedDelay from './randomizedDelay';
+import saveAsJson from './saveAsJson';
+import {
+  CustomError,
+  AuthError,
+  FetchFailedError,
+  ParsingError,
+  CookieExtractionError,
+} from './errors';
+
+export default {
+  formatQuery,
+  randomizedDelay,
+  saveAsJson,
+  CustomError,
+  AuthError,
+  FetchFailedError,
+  ParsingError,
+  CookieExtractionError,
+};

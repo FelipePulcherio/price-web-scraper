ce6c068c15aadd815baee398388c26de14ffe33d
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-10-04 16:19:08 -0400

    Replace testSearch with search + Add job assignment

--- backend/src/api/routes/search.ts
+++ backend/src/api/routes/search.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { searchItemByString } from '@/database/operations/dbRead';
 import resFormatter from '@/helpers/apiResponseFormatter';
 import middlewares from '../middlewares';
+import scraperScheduler from '@/schedule/scraperScheduler';
 
 const route = Router();
 
@@ -59,42 +60,28 @@ function searchRoute(app: Router): void {
 
         console.log(`GET /api/v1/search?q=${search}`);
 
+        // 1) Look up in DB
         const fetchedItems = await searchItemByString(search, pageSize, page);
-        // console.log(fetchedItems);
+
+        if (fetchedItems && fetchedItems.length > 0) {
+          res
+            .status(200)
+            .json(
+              resFormatter(true, ['Items fetched successfully'], fetchedItems)
+            );
+          return;
+        }
+
+        // 2) Not in DB. Create new Unique Job. If nothing happens, it
+        //  means that the job is already running. User should poll.
+        await scraperScheduler
+          .create('searchAllStores', { query: search })
+          .unique({ 'data.query': search }, { insertOnly: true })
+          .save();
 
         res
-          .status(200)
-          .json(
-            resFormatter(true, ['Items fetched successfully'], fetchedItems)
-          );
-      } catch (err) {
-        // Pass errors to middlewares.errorHandler
-        next(err);
-      }
-    }
-  );
-
-  // GET /api/v1/testSearch?q=
-  // Used on regular searches. Use pages with 24 items
-  route.get(
-    '/testSearch',
-    middlewares.validateSearch,
-    async (req: Request, res: Response, next: NextFunction) => {
-      try {
-        const search = req.query.q as string;
-        const pageSize = 24;
-        const page = parseInt(req.query.page as string, 10) || 1;
-
-        console.log(`GET /api/v1/testSearch?q=${search}`);
-
-        const fetchedItems = await searchItemByString(search, pageSize, page);
-        // console.log(fetchedItems);
-
-        res
-          .status(200)
-          .json(
-            resFormatter(true, ['Items fetched successfully'], fetchedItems)
-          );
+          .status(202)
+          .json(resFormatter(false, [`Searching for "${search}"`], []));
       } catch (err) {
         // Pass errors to middlewares.errorHandler
         next(err);

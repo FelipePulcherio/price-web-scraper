03a10712584f4b8e6e40905d5bd0938803c69453
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-10-05 20:16:01 -0400

    Replace startAgenda with startScraperScheduler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,11 @@ import express, { Express } from 'express';
 import process from 'process';
 import config from '@/config';
 import { startAgenda } from './schedule/scheduler';
+import { startScraperScheduler } from './schedule/scraperScheduler';
+import bestBuyCaSearch from './services/stores/bestBuyCa/bestBuyCaSearch';
+import searchAllStores from './services/stores/searchAllStores';
+import visionsElectronicsCaSearch from './services/stores/visionsElectronicsCa/visionsElectronicsCaSearch';
+import londonDrugsCaSearch from './services/stores/londonDrugsCa/londonDrugsCaSearch';
 
 async function startServer() {
   const app: Express = express();
@@ -14,9 +19,9 @@ async function startServer() {
   });
 
   function gracefulShutdown(): void {
-    console.log('Shutting down gracefully...');
+    console.log('[Server]: Shutting down gracefully...');
     server.close(() => {
-      console.log('Server closed');
+      console.log('[Server]: Server closed.');
       process.exit(0);
     });
   }
@@ -24,7 +29,21 @@ async function startServer() {
   process.on('SIGINT', gracefulShutdown);
   process.on('SIGTERM', gracefulShutdown);
 
-  startAgenda();
+  startScraperScheduler();
+  // await searchAllStores({ query: 'tv' });
+  // await searchAllStores({ query: 'tv samsung' });
+  // await searchAllStores({ query: 'tv lg 2025' });
+  // await searchAllStores({ query: 'tv tcl' });
+  // await searchAllStores({ query: 'tv hisense' });
+  // await searchAllStores({ query: 'tv roku' });
+  // await searchAllStores({ query: 'tv smart' });
+  // await searchAllStores({ query: 'ipad' });
+  // await searchAllStores({ query: 'ipad 10' });
+  // await searchAllStores({ query: 'ipad 11' });
+  // await searchAllStores({ query: 'computer' });
+  // await searchAllStores({ query: 'laptop' });
+  // await searchAllStores({ query: 'iphone' });
+  // await searchAllStores({ query: 'iphone 16' });
 }
 
 // Function calls

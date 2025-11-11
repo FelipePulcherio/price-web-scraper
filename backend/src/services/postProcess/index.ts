30cd0c96fba0584a5a9877e3baeb22c3119ffe61
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-11-11 14:15:47 -0400

    Create default export

--- backend/src/services/postProcess/index.ts
+++ backend/src/services/postProcess/index.ts
@@ -1,19 +1,5 @@
-import imagePostProcess from './images/images';
+import createItemImages from './images/createItemImages';
 
-export default async function (itemIds: number[]): Promise<void> {
-  try {
-    console.log(`[Post Process]: Starting for ${itemIds.length} items.`);
-
-    // TODO: Implement image processing here (Cloudinary upload, etc.)
-    for (const id of itemIds) {
-      await imagePostProcess();
-      console.log(`[Post Process]: working on item ID ${id}`);
-      // Future: upload image, update DB, etc.
-    }
-
-    console.log('[Post Process]: Completed successfully.');
-  } catch (err) {
-    console.error('[Post Process]: Failed.', err);
-    throw err;
-  }
-}
+export default {
+  createItemImages,
+};

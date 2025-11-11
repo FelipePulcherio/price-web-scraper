612d93567eb97eaeb53428c592a05953e9209f2c
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-11-11 14:20:34 -0400

    Change job name

--- backend/src/agenda/jobs/postProcess.job.ts
+++ backend/src/agenda/jobs/postProcess.job.ts
@@ -1,22 +1,22 @@
 import { agendaPostProcess } from '@/agenda';
 import postProcess from '@/services/postProcess';
 
-agendaPostProcess.define('postProcess', async (job) => {
+agendaPostProcess.define('createItemImages', async (job) => {
   const { itemIds } = job.attrs.data as { itemIds: number[] };
 
   try {
     console.log(
-      `[Agenda - Post Process]: Running job postProcess for ${itemIds.length} items.`
+      `[Agenda - Post Process]: Running job createItemImages for ${itemIds.length} items.`
     );
 
-    await postProcess(itemIds);
+    await postProcess.createItemImages(itemIds);
 
     console.log(
-      `[Agenda - Post Process]: Finished job for ${itemIds.length} items.`
+      `[Agenda - Post Process]: Finished createItemImages job for ${itemIds.length} items.`
     );
   } catch (err) {
     console.error(
-      `[Agenda - Post Process]: Failed job for ${itemIds.length} items.`,
+      `[Agenda - Post Process]: Failed createItemImages job for ${itemIds.length} items.`,
       err
     );
   }

4023f00a3b4bc015666f11f98f44b35475ab75f7
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-11-14 10:12:06 -0400

    Convert to ESM. Remove path aliases

--- backend/src/database/operations/dbUpdate.ts
+++ backend/src/database/operations/dbUpdate.ts
@@ -1,6 +1,5 @@
-import { Prisma } from '@prisma/client';
-import prisma from '@/loaders/prisma.js';
-import { IImage, IItem } from '../../interfaces/interfaces.js';
+import prisma from '../../loaders/prisma.js';
+import { IImage } from '../../interfaces/interfaces.js';
 
 // FUNCTIONS
 export async function updateImageById(id: number, data: IImage): Promise<void> {

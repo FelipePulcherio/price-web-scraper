ae8b79a37f18e84b58baf62f9cc8450e3d48687a
FelipePulcherio <felipe.pulcherio1@gmail.com>
2025-11-14 18:27:47 -0400

    Rename job and function

--- backend/src/agenda/jobs/postProcess.job.ts
+++ backend/src/agenda/jobs/postProcess.job.ts
@@ -1,22 +1,22 @@
 import { agendaPostProcess } from '../index.js';
 import postProcess from '../../services/postProcess/index.js';
 
-agendaPostProcess.define('createItemImages', async (job) => {
+agendaPostProcess.define('imagesFromScraping', async (job) => {
   const { itemIds } = job.attrs.data as { itemIds: number[] };
 
   try {
     console.log(
-      `[Agenda - Post Process]: Running job createItemImages for ${itemIds.length} items.`
+      `[Agenda - Post Process]: Running job imagesFromScraping for ${itemIds.length} items.`
     );
 
-    await postProcess.maintainItemImages(itemIds);
+    await postProcess.imagesFromScraping(itemIds);
 
     console.log(
-      `[Agenda - Post Process]: Finished createItemImages job for ${itemIds.length} items.`
+      `[Agenda - Post Process]: Finished imagesFromScraping job for ${itemIds.length} items.`
     );
   } catch (err) {
     console.error(
-      `[Agenda - Post Process]: Failed createItemImages job for ${itemIds.length} items.`,
+      `[Agenda - Post Process]: Failed imagesFromScraping job for ${itemIds.length} items.`,
       err
     );
   }
